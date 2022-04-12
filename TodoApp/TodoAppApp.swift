c872ec462a0bb0bf85a4f9023568a9e99355db21
naveen4yalla <yvnaveen3120@gmail.com>
2022-04-11 18:14:56 -0700

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by Naveen Yalla on 4/11/22.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
